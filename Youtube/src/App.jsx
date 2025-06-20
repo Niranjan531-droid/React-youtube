87f639661da586b7502430985c119f7c985ff1ce
Niranjan531-droid <mailtoniranjantp@gmail.com>
2025-06-20 20:13:12 +0530

    app.jsx is completed

--- Youtube/src/App.jsx
+++ Youtube/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Sidebar from './Components/Sidebar';
+import Home from './Pages/Home';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+  return(
+    <div className='bg-black min-h-screen text-white'>
+      <Navbar />
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='flex'>
+        <Sidebar />
+
+        <main className='flex-1 p-4'>
+          <Home />
+        </main>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
   )
 }
 
-export default App
+export default App;
